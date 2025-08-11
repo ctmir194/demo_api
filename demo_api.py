207893e0f3b3382aae535bb5f49533676d27cf8b
ctmri194 <ctmri194@gmail.com>
2025-08-11 14:47:01 +0530

    "Some more code"

--- demo_api.py
+++ demo_api.py
@@ -6,8 +6,22 @@ from pydantic import BaseModel, validator
 app = FastAPI()
 
 class XRay(BaseModel):
-    file: File
+    filename: str
+    content: bytes
+
+    @validator('filename')
+    def filename_validator(cls, value):
+        if not value.lower().endswith('.dcm'):
+            raise ValueError("File extension should be '.dcm'")
+        return value
+    
+    @validator('content')
+    def content_validator(cls, value):
+        if len(value) < 132 or value[128:132] != b"DICM":            ## he b prefix means this is a bytes literal, not a string. ## first 128 bytes are "preamble"                                                    (can be anything or zero-filled) + 4 for DICM so length shold be atleast 132
+            raise ValueError("File does not appear to a DICOM file")
+        return value
     
 
-# app.post('/generate')
-# def generate(file: UploadFile = File(...)):
+app.post('/generate')
+def generate(file: UploadFile = File(...)):
+    
